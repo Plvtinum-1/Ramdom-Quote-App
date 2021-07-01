8d24a7317ba98e4590334fc0f2e5649685d79613
Ismail Dahri <dahriiismail@gmail.com>
2021-07-01 12:09:04 +0100

    commited

--- src/App.js
+++ src/App.js
@@ -5,14 +5,12 @@ import 'bootstrap/dist/css/bootstrap.css';
 import { FaTwitter, FaTumblr, FaFacebookF } from 'react-icons/fa';
 import { GoQuote } from 'react-icons/go';
 
-
-
 class App extends React.Component{
   constructor(props){
     super(props);
     this.state = {
       quote: 'Remember its only 40% of your potential when you feel tired',
-      authorName: 'Unknown Programmer',
+      author: 'Unknown Programmer',
     }
    this.handleChange = this.handleChange.bind(this)
   }
@@ -20,9 +18,10 @@ class App extends React.Component{
   handleChange(){
     this.setState(() => ({
       quote: Quote.getQuote().text,
-      authorName: Quote.getQuote().author
-    } ))
+      author: Quote.getQuote().author
+    }))
   }
+
   
   componentDidUpdate(){
     const changingColor = `rgb(${Math.floor(Math.random() * 255)}, ${Math.floor(Math.random() * 255)}, ${Math.floor(Math.random() * 255)})`;
@@ -40,19 +39,20 @@ class App extends React.Component{
     document.body.style.backgroundColor = changingColor;
   }
   render(){
+  
     return ( 
       <div className="App" id="quote-box">
         <div className="firstDiv" id="text"> 
             <quote className="firstQ"><GoQuote className="go" /> {this.state.quote}</quote> 
         </div>
         <div className="authorQuote" id="author">
-          <quote className='secondQ'>- {this.state.authorName}</quote>
+          <quote className='secondQ'>- {this.state.author}</quote>
         </div>
         <div className="secondDiv">
-            <button className="btn"><a id="tweet-quote" target="_blank" href="https://twitter.com/intent/tweet"><FaTwitter className="icons" /></a></button>
+        <button className="btn"><a id="tweet-quote" target="_blank" href="https://twitter.com/intent/tweet"><FaTwitter className="icons" /></a></button>
             <button className="btn"><a target="_blank" href="https://twitter.com/intent/tweet"><FaTumblr className="icons" /></a></button>
             <button className="btn"><a target="_blank" href="https://twitter.com/intent/tweet"><FaFacebookF className="icons" /></a></button>
-            
+
             <button onClick={this.handleChange} className="new btn" id="new-quote">New Quote</button>
         </div> 
       </div>    
